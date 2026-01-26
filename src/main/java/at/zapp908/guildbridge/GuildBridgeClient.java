a90b77880b1aab6e1761de6f5956547d634affe5
Gehrig <141294967+zapp9080@users.noreply.github.com>
2026-01-25 21:32:03 -0600

    Initial commit

---
+++ src/main/java/at/zapp908/guildbridge/GuildBridgeClient.java
@@ -0,0 +1,10 @@
+package at.zapp908.guildbridge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GuildBridgeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/at/zapp908/guildbridge/GuildBridgeDataGenerator.java
@@ -0,0 +1,11 @@
+package at.zapp908.guildbridge;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GuildBridgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
